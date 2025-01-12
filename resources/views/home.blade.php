5f84fb4fa2ad3b6dfb64410ba78b125ea6e8a0b4
Manish Maharjan <manishnewar26@gmail.com>
2025-01-12 12:28:33 +0000

    Layout separated with default slot added and configured in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-
-<body>
-    @include('Components.nav')
-    <h1>Hello from the home page</h1>
-</body>
-
-</html>
+<x-layout>
+    <h1>Hello from the Home page</h1>
+</x-layout>
